5f7e540af29615c60ddef19c1637239d051e2515
Donggyu-Kim <kimdg000411@gmail.com>
2024-08-26 15:58:11 +0900

    model 초기화

--- accounts/models.py
+++ accounts/models.py
@@ -1,20 +1,2 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=30)
-    phone_number = models.CharField(max_length=15)
-
-# User가 생성될 때 Profile도 자동 생성되도록 신호 처리기 추가
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
