d0702d0108719bcda3b604386306ff7dd1056f7b
Donggyu-Kim <kimdg000411@gmail.com>
2024-08-27 14:21:58 +0900

    forum/urls.py 수정, models.py 작성

--- forum/models.py
+++ forum/models.py
@@ -1,1 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from stocks.models import Stock  # stocks 앱의 Stock 모델을 import
+
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)  # stocks 앱의 Stock 모델을 참조
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    content = models.TextField()
+    likes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.title}"
