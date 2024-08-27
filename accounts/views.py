44c95694d89782d4ed64a7edaa38808d47457218
Donggyu-Kim <kimdg000411@gmail.com>
2024-08-27 10:26:24 +0900

    accounts/views.py작성 및 템플릿 추가, readme파일 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,36 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView, DetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import UserProfileForm
 
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
 
-class accountSignupView():
-    pass
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
 
+class CustomLogoutView(LogoutView):
+    next_page = 'login'
 
-class accountLoginView():
-    pass
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'accounts/profile.html'
+    context_object_name = 'user'
 
+    def get_object(self):
+        return self.request.user
 
-class accountProfileView():
-    pass
+class EditProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'accounts/edit_profile.html'
+    success_url = reverse_lazy('profile')
+
+    def get_object(self):
+        return self.request.user
 
