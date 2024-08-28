0b87ecad23bd1f19dd09cbafc9c578253626e4f7
Donggyu-Kim <kimdg000411@gmail.com>
2024-08-28 16:54:50 +0900

    forum views.py, templates 재작성

--- forum/urls.py
+++ forum/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('<str:ticker>/', views.ForumMainView.as_view(), name='forum_list'), # 특정 기업 토론방 메인
     path('<str:ticker>/create/', views.PostCreateView.as_view(), name='post_create'),  # 글 작성
     path('<str:ticker>/<int:post_id>/', views.PostReadView.as_view(), name='post_read'),  # 글 읽기
-    path('<str:ticker>/<int:post_id>/detail/', views.PostDetailView.as_view(), name='post_detail'),  # 글 추천, 조회수
     path('<str:ticker>/<int:post_id>/update/', views.PostUpdateView.as_view(), name='post_update'),  # 글 수정
     path('<str:ticker>/<int:post_id>/delete/', views.PostDeleteView.as_view(), name='post_delete'),  # 글 삭제
     path('<str:ticker>/<int:post_id>/comments/', views.PostCommentListView.as_view(), name='post_comment_list'),  # 댓글 리스트
--- forum/views.py
+++ forum/views.py
@@ -1,23 +1,24 @@
-from django.views import View
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from .models import Post, Comment
-from .forms import PostForm, CommentForm
+from stocks.models import Stock
+from .forms import PostForm, CommentForm  # 폼을 별도로 정의해야 합니다
 
 class ForumMainView(ListView):
     model = Post
     template_name = 'forum/forum_list.html'
     context_object_name = 'posts'
+    paginate_by = 10  # 페이지당 10개의 게시글
 
     def get_queryset(self):
-        ticker = self.kwargs.get('ticker')
-        return Post.objects.filter(stock_ticker=ticker)
+        self.stock = get_object_or_404(Stock, ticker=self.kwargs['ticker'])
+        return Post.objects.filter(stock_ticker=self.stock).order_by('-created_at')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['ticker'] = self.kwargs.get('ticker')
+        context['stock'] = self.stock
         return context
 
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -27,7 +28,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        form.instance.stock_ticker = self.kwargs['ticker']
+        form.instance.stock_ticker = get_object_or_404(Stock, ticker=self.kwargs['ticker'])
         return super().form_valid(form)
 
     def get_success_url(self):
@@ -40,39 +41,32 @@ class PostReadView(DetailView):
     pk_url_kwarg = 'post_id'
 
     def get_object(self):
-        return get_object_or_404(Post, id=self.kwargs['post_id'], stock_ticker=self.kwargs['ticker'])
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'forum/post_detail.html'
-    context_object_name = 'post'
-    pk_url_kwarg = 'post_id'
-
-    def get_object(self):
         obj = super().get_object()
         obj.views += 1
         obj.save()
         return obj
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = 'forum/post_update.html'
     pk_url_kwarg = 'post_id'
 
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
     def get_success_url(self):
-        return reverse_lazy('forum:post_read', kwargs={'ticker': self.kwargs['ticker'], 'post_id': self.object.id})
+        return reverse_lazy('forum:post_read', kwargs={'ticker': self.kwargs['ticker'], 'post_id': self.object.pk})
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'forum/post_delete.html'
     pk_url_kwarg = 'post_id'
 
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
     def get_success_url(self):
         return reverse_lazy('forum:forum_list', kwargs={'ticker': self.kwargs['ticker']})
@@ -83,7 +77,13 @@ class PostCommentListView(ListView):
     context_object_name = 'comments'
 
     def get_queryset(self):
-        return Comment.objects.filter(post_id=self.kwargs['post_id'])
+        self.post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        return Comment.objects.filter(post=self.post).order_by('created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = self.post
+        return context
 
 class PostCommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
@@ -92,20 +92,21 @@ class PostCommentCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
-        form.instance.post_id = self.kwargs['post_id']
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['post_id'])
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse_lazy('forum:post_read', kwargs={'ticker': self.kwargs['ticker'], 'post_id': self.kwargs['post_id']})
 
-class PostCommentDetailView(LoginRequiredMixin, UpdateView):
+class PostCommentDetailView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
     template_name = 'forum/post_comment_detail.html'
     pk_url_kwarg = 'comment_id'
 
-    def get_queryset(self):
-        return Comment.objects.filter(author=self.request.user)
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
 
     def get_success_url(self):
-        return reverse_lazy('forum:post_read', kwargs={'ticker': self.kwargs['ticker'], 'post_id': self.kwargs['post_id']})
+        return reverse_lazy('forum:post_read', kwargs={'ticker': self.kwargs['ticker'], 'post_id': self.object.post.pk})
