cead950495ba7d2a1a9d46889018988f04ddc5d9
Donggyu-Kim <kimdg000411@gmail.com>
2024-08-28 12:51:42 +0900

    stocks/views.py 삭제

--- stocks/views.py
+++ stocks/views.py
@@ -11,29 +11,7 @@ openai.api_key = os.getenv('OPEN_API_KEY')
 class StocksSearchView(TemplateView):
     template_name = 'stocks/stocks_search.html'
 
-    def post(self, request, *args, **kwargs):
-        search_term = request.POST.get('search_term')
-        try:
-            ticker = self.get_ticker_from_company_name(search_term)
-            stock = self.validate_stock(ticker)
-            self.save_to_database(ticker, search_term)
-            return redirect('stocks:intro', ticker=ticker)
-        except Exception as e:
-            messages.error(request, f"'{search_term}'에 해당하는 주식 티커를 찾을 수 없습니다. 다시 시도해주세요.")
-            return self.get(request, *args, **kwargs)
 
-    def get_ticker_from_company_name(self, company_name):
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant that converts company names to stock ticker symbols."},
-                {"role": "user", 
-                 "content": f"Convert {company_name} to a ticker so that I can search for stocks in the Python yfinance library. Please give me only the ticker as the answer."}
-            ],
-            max_tokens=60,
-        )
-        return response.choices[0].message['content'].strip()
-    
     def validate_stock(self, ticker):
         stock = yf.Ticker(ticker)
         if not stock.info.get('symbol'):
