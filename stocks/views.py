92920abe19e582fd7386590635a28bda101541e6
Donggyu-Kim <kimdg000411@gmail.com>
2024-08-29 11:32:44 +0900

    googletrans로 번역 기능 추가, 너무 느림, 하루 15000자 제한

--- stocks/views.py
+++ stocks/views.py
@@ -2,7 +2,7 @@ from django.views.generic import ListView, DetailView
 from django.db.models import Q
 from .models import Stock
 import yfinance as yf
-
+from googletrans import Translator
 
 class StocksSearchView(ListView):
     model = Stock
@@ -24,6 +24,7 @@ class StocksSearchView(ListView):
         context['query'] = self.request.GET.get('q', '')
         return context
 
+
 class StockDetailView(DetailView):
     model = Stock
     template_name = 'stocks/stocks_detail.html'
@@ -36,8 +37,14 @@ class StockDetailView(DetailView):
         # Yahoo Finance에서 주식 정보 가져오기
         stock = yf.Ticker(ticker)
 
-        # 기업 소개
-        context['company_info'] = stock.info.get('longBusinessSummary', 'No information available')
+        # 번역기 설정
+        translator = Translator()
+        target_language = 'ko'
+
+        # 기업 소개 번역
+        company_info = stock.info.get('longBusinessSummary', 'No information available')
+        translated_company_info = translator.translate(company_info, dest=target_language).text
+        context['company_info'] = translated_company_info
 
         # 뉴스
         context['news'] = stock.news
@@ -46,10 +53,14 @@ class StockDetailView(DetailView):
         hist = stock.history(period="1mo")
         context['chart_data'] = hist['Close'].to_json(date_format='iso')
 
-        # 재무제표
+        # 재무제표 번역
         financials = stock.financials
         if not financials.empty:
-            context['financials'] = financials.to_html(classes='table table-striped')
+            # 계정과목을 한국어로 번역합니다.
+            translated_financials = financials.rename(
+                lambda x: translator.translate(x, dest=target_language).text, axis='index'
+            )
+            context['financials'] = translated_financials.to_html(classes='table table-striped')
         else:
             context['financials'] = "No financial data available"
 
