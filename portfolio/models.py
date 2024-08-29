1fcc56129a197e3bbe2e3a1d7855d23bb8eed239
Donggyu-Kim <kimdg000411@gmail.com>
2024-08-29 21:23:50 +0900

    portfolio models.py 생성

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Portfolio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='portfolios')
+    name = models.CharField(max_length=255)  # 포트폴리오 이름
+    created_at = models.DateTimeField(auto_now_add=True)  # 생성일
+
+    def __str__(self):
+        return self.name
+
+class PortfolioStock(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='stocks')
+    stock = models.ForeignKey('Stock', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()  # 보유 주식 수량
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)  # 매수 가격
+    created_at = models.DateTimeField(auto_now_add=True)  # 추가된 날짜
+
+    def __str__(self):
+        return f"{self.stock.ticker} in {self.portfolio.name}"
+
+    class Meta:
+        unique_together = ['portfolio', 'stock']  # 포트폴리오에 동일 주식을 중복 추가 X
