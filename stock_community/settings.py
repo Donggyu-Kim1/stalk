583ad10587bbf59e6aea48363366915653bec875
Donggyu-Kim <kimdg000411@gmail.com>
2024-08-29 20:31:46 +0900

    이미지 로드 방식 변경

--- stock_community/settings.py
+++ stock_community/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import os
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,11 +93,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
-
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / 'media'
