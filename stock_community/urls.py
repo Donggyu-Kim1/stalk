03495ac24ea03dd0fcd61571b23c74b9a8c72622
Donggyu-Kim <kimdg000411@gmail.com>
2024-08-29 09:53:24 +0900

    settings.py, 프로젝트 url media url 추가

--- stock_community/urls.py
+++ stock_community/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +12,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('forum/', include('forum.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
